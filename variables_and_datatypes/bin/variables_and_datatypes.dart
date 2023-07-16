474962493668b351655326232e5633b9951e7c53
Carl Mensah <carlnktmensah@gmail.com>
2023-07-16 15:06:17 +0000

    change 1

--- variables_and_datatypes/bin/variables_and_datatypes.dart
+++
@@ -1,5 +0,0 @@
-import 'package:variables_and_datatypes/variables_and_datatypes.dart' as variables_and_datatypes;
-
-void main(List<String> arguments) {
-  print('Hello world: ${variables_and_datatypes.calculate()}!');
-}
